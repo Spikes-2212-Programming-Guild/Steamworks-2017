00b7cd12bbda7f051f46ad16526d6c1d5317a6e2
roeeiit1 <roeeiit1@gmail.com>
2017-01-17 20:58:12 +0200

    added Move, Turn Right And Drop Gear Auto

---
+++ src/org/usfirst/frc/team2212/robot/commands/MoveTurnRightAndDropGearAuto.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team2212.robot.commands;
+
+import java.util.function.Supplier;
+
+import com.spikes2212.dashboard.ConstantHandler;
+import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTank;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class MoveTurnRightAndDropGearAuto extends CommandGroup {
+
+	public MoveTurnRightAndDropGearAuto(TankDrivetrain drivetrain) {
+		Supplier<Double> MovingSpeed = ConstantHandler.addConstantDouble("MovigSpeed", 0.5);
+		Supplier<Double> TurningSpeed = ConstantHandler.addConstantDouble("TurningSpeed", -0.5);
+		Supplier<Double> SecendMovementTime = ConstantHandler.addConstantDouble("SecendMovementTime", 1);
+		Supplier<Double> FirstMovementTime = ConstantHandler.addConstantDouble("FirstMovementTime", 4);
+		addSequential(new DriveTank(drivetrain, MovingSpeed, MovingSpeed), FirstMovementTime.get());
+		addSequential(new Orienate(TurningSpeed.get()));
+		addSequential(new DriveTank(drivetrain, MovingSpeed, MovingSpeed), SecendMovementTime.get());
+	}
+}
