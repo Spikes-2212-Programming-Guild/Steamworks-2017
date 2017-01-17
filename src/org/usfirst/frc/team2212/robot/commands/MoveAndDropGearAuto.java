fd7adf426472cc99a8ef29fcacb1f77295d3773c
roeeiit1 <roeeiit1@gmail.com>
2017-01-17 20:46:16 +0200

    added move and add drop gear auto

---
+++ src/org/usfirst/frc/team2212/robot/commands/MoveAndDropGearAuto.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team2212.robot.commands;
+
+import java.util.function.Supplier;
+
+import com.spikes2212.dashboard.ConstantHandler;
+import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTank;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class MoveAndDropGearAuto extends CommandGroup {
+
+    public MoveAndDropGearAuto(TankDrivetrain drivetrain) {
+    	Supplier<Double> Speed = ConstantHandler.addConstantDouble("Speed", 0.5);
+    	Supplier<Double> Time = ConstantHandler.addConstantDouble("Time", 3);
+        addSequential(new DriveTank(drivetrain, Speed, Speed), Time.get());
+    }
+}
