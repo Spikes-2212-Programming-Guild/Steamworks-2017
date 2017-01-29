5a8ca8b731c4bf7f324fc2d1abed6acd1b4ac438
omri cohen <omrichoen@gmail.com>
2017-01-29 18:33:29 +0200

    added Picker

---
+++ src/org/usfirst/frc/team2212/robot/subsystems/Picker.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team2212.robot.subsystems;
+
+import com.spikes2212.genericsubsystems.LimitedSubsystem;
+
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Picker extends LimitedSubsystem {
+	
+	private SpeedController motor;
+	
+	public Picker(SpeedController motor) {
+		this.motor=motor;
+	}
+	
+	@Override
+	public boolean isMin() {
+		return false;
+	}
+
+	@Override
+	public boolean isMax() {
+		return false;
+	}
+
+	@Override
+	public PIDSource getPIDSource() {
+		return null;
+	}
+
+	@Override
+	protected void move(double speed) {
+		motor.set(speed);
+	}
+}
+
