2321f35c4023969512d7b2da01afce5bcc60fe25
omri cohen <omrichoen@gmail.com>
2017-01-29 18:10:32 +0200

    added Climber

---
+++ src/org/usfirst/frc/team2212/robot/subsystems/Climber.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team2212.robot.subsystems;
+
+
+import com.spikes2212.genericsubsystems.LimitedSubsystem;
+
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Climber extends LimitedSubsystem{
+	SpeedController motor;
+	public Climber(SpeedController motor) {
+		this.motor=motor;
+	}
+
+	@Override
+	public boolean isMin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isMax() {
+		// TODO Check how to get the isMax and write it here
+		return false;
+	}
+
+	@Override
+	public PIDSource getPIDSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void move(double speed) {
+		motor.set(speed);
+	}
+}
+
