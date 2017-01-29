e3045bed2386bfa3a26a21ca86b4e458452ad221
omri cohen <omrichoen@gmail.com>
2017-01-29 19:00:53 +0200

    added Shooter

---
+++ src/org/usfirst/frc/team2212/robot/subsystems/Shooter.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team2212.robot.subsystems;
+
+import com.spikes2212.genericsubsystems.LimitedSubsystem;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Shooter extends LimitedSubsystem {
+
+	SpeedController motor;
+	Encoder encoder;
+	
+	public Shooter(SpeedController motor, Encoder encoder) {
+		this.encoder=encoder;
+		this.motor=motor;
+	}
+	@Override
+	public boolean isMin() {
+		return false;
+	}
+
+	@Override
+	public boolean isMax() {
+		return false;
+	}
+
+	@Override
+	public PIDSource getPIDSource() {
+		return encoder;
+	}
+
+	@Override
+	protected void move(double speed) {
+		motor.set(speed);
+	}
+}
+
