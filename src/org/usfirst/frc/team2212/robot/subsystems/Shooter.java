8aafe1cfb0786df9610083f70b2208b83cdf589a
omri cohen <omrichoen@gmail.com>
2017-02-02 18:00:10 +0200

    set the encoder's PIDSourceType and DistancePerPulse

--- src/org/usfirst/frc/team2212/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team2212/robot/subsystems/Shooter.java
@@ -1,9 +1,13 @@
 package org.usfirst.frc.team2212.robot.subsystems;
 
+import java.util.function.Supplier;
+
+import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.genericsubsystems.LimitedSubsystem;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -15,10 +19,13 @@ public class Shooter extends LimitedSubsystem {
 
 	private TalonSRX motor;
 	private Encoder encoder;
+	private Supplier<Double> distancePerPulse = ConstantHandler.addConstantDouble("distancePerPulse", 1); //TODO check the real value
 
 	public Shooter(TalonSRX motor, Encoder encoder) {
 		this.encoder = encoder;
 		this.motor = motor;
+		encoder.setPIDSourceType(PIDSourceType.kRate);
+		encoder.setDistancePerPulse(distancePerPulse.get());
 	}
 
 	@Override
