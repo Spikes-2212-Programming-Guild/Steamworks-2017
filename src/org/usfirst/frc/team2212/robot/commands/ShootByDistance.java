170d0021ca0ba38b47c63106ef7acdc7e302927f
omri cohen <omrichoen@gmail.com>
2017-02-04 19:45:23 +0200

    removed some of the cancer

--- src/org/usfirst/frc/team2212/robot/commands/ShootByDistance.java
+++ src/org/usfirst/frc/team2212/robot/commands/ShootByDistance.java
@@ -7,23 +7,39 @@ import org.usfirst.frc.team2212.robot.subsystems.Shooter;
 import com.spikes2212.dashboard.ConstantHandler;
 
 public class ShootByDistance extends ShootBySpeed {
-	static Supplier<Double> cameraAngle = ConstantHandler.addConstantDouble("cameraAngle", 20);
-	static Supplier<Double> boilerHeight = ConstantHandler.addConstantDouble("boilerHeight", 250); // cm
-	
+	public static final double SHOOTING_ANGLE = 20; // FIXME find the real number
+	public static final double BOILER_HEIGHT = 2.74; // meter FIXME find the real number
+	public static final double WHEEL_DIAMETER = 4; // inches FIXME find the real number
+	public static final int ENCODER_TICKS = 1440; // FIXME find the real number
+
 	public ShootByDistance(Shooter shooter, Supplier<Double> distance, double KP, double KI, double KD) {
-		super(shooter,calculateSpeedByDistance(distance),KP,KI,KD);
+		super(shooter, () -> calculateSpeedByDistance(distance.get()), KP, KI, KD);
 	}
-	private static Supplier<Double> calculateSpeedByDistance(Supplier<Double> distance){
+
+	private static double calculateSpeedByDistance(double distance) {
 		/*
-		 * Physics: Vx = speed*sin(cameraAngle) Vy = speed*cos(cameraAngle)
-		 * distance = t*Vx=t*speed*sin(cameraAngle) t=d/(speed*sin(cameraAngle))
-		 * boilerHeight = t*Vy-5t^2=t*speed*cos(cameraAngle)-5t^2=
-		 * distance*tan(cameraAngle)-5d^2/(speed*sin(cameraAngle))^2
-		 * 5d^2/(speed*sin(cameraAngle))^2=distance*tan(cameraAngle)- boilerHeight
-		 * speed^2=5*distance^2/(sin(cameraAngle)^2*(distance*tan(cameraAngle)- boilerHeight))
-		 * speed=root(5*distance^2/(sin(cameraAngle)^2*(distance*tan(cameraAngle)- boilerHeight)))
+		 * Physics: Vx = speed*cos(shootingAngle) Vy = speed*sin(shootingAngle)
+		 * 
+		 * distance = t*Vx=t*speed*cos(shootingAngle)
+		 * t=distance/(speed*cos(shootingAngle)) boilerHeight =
+		 * t*Vy-4.9t^2=t*speed*sin(shootingAngle)-4.9t^2=
+		 * distance*tan(shootingAngle)-5d^2/(speed*cos(shootingAngle))^2
+		 * 
+		 * 5d^2/(speed*cos(shootingAngle))^2=distance*tan(shootingAngle)-
+		 * boilerHeight
+		 * 
+		 * speed^2=5*distance^2/(cos(shootingAngle)^2*(distance*tan(
+		 * shootingAngle)- boilerHeight))
+		 * 
+		 * speed=root(5*distance^2/(cos(shootingAngle)^2*(distance*tan(
+		 * shootingAngle )- boilerHeight)))
 		 */
-		return ()->Math.sqrt(5*distance.get()*distance.get()/(Math.sin(cameraAngle.get()*Math.PI/180)*(Math.sin(cameraAngle.get()*Math.PI/180)*(distance.get() * Math.tan(cameraAngle.get()*Math.PI/180)- boilerHeight.get()))));
+		double angleInRadians = Math.toRadians(SHOOTING_ANGLE);
+		double meterToTicks = ENCODER_TICKS / (0.0254 * Math.PI * WHEEL_DIAMETER);
+		
+		double speedInMeter = Math.sqrt(4.9 * Math.pow(distance / (Math.cos(angleInRadians)), 2)
+				/ (distance * Math.tan(angleInRadians) - BOILER_HEIGHT));
+		return speedInMeter * meterToTicks;
 	}
 
 }
