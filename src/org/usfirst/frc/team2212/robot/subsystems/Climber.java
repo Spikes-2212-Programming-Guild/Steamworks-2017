dfb80dd2d227877008cbde84a6c43e9253935e6e
ThinkRedstone <ittai11@gmail.com>
2017-02-08 21:08:15 +0200

    Cleaned up Climber

--- src/org/usfirst/frc/team2212/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team2212/robot/subsystems/Climber.java
@@ -9,50 +9,32 @@ import com.spikes2212.genericsubsystems.LimitedSubsystem;
 import edu.wpi.first.wpilibj.PIDSource;
 
 public class Climber extends LimitedSubsystem {
-	private CANTalon motor1, motor2;
-	public static final Supplier<Double> MAX_CURRENT = ConstantHandler.addConstantDouble("Climber-MAX_CURRENT", 20);
+    private CANTalon motor;
+    public static final Supplier<Double> MAX_CURRENT = ConstantHandler.addConstantDouble("Climber-MAX_CURRENT", 20);
 
-	public Climber(CANTalon motor1, CANTalon motor2) {
-		this.motor1 = motor1;
-		this.motor2 = motor2;
-	}
+    public Climber(CANTalon motor) {
+        this.motor = motor;
+    }
 
-	@Override
-	public boolean isMin() {
-		// TODO Auto-generated method sub
-		return false;
-	}
+    @Override
+    public boolean isMin() {
+        // TODO Auto-generated method sub
+        return false;
+    }
 
-	public double getCurrent1() {
-		if (motor1 != null)
-			return motor1.getOutputCurrent();
-		return 0;
-	}
+    @Override
+    public boolean isMax() {
+        return motor.getOutputCurrent() >= MAX_CURRENT.get();
+    }
 
-	public double getCurrent2() {
-		if (motor2 != null)
-			return motor2.getOutputCurrent();
-		return 0;
-	}
+    @Override
+    public PIDSource getPIDSource() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public boolean isMax() {
-		if (Math.max(motor1.getOutputCurrent(), motor2.getOutputCurrent()) >= MAX_CURRENT.get()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	@Override
-	public PIDSource getPIDSource() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected void move(double speed) {
-		motor1.set(speed);
-		motor2.set(speed);
-	}
+    @Override
+    protected void move(double speed) {
+        motor.set(speed);
+    }
 }
