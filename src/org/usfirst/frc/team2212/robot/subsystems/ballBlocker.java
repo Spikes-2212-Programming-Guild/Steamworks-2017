35b307f74f6e1df323c742a5d465f0107824b4d8
Leon <Leon@192.168.1.110>
2017-02-08 17:21:39 +0200

    ballBlocker subsystem added

---
+++ src/org/usfirst/frc/team2212/robot/subsystems/ballBlocker.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team2212.robot.subsystems;
+
+import com.spikes2212.genericsubsystems.LimitedSubsystem;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class ballBlocker extends LimitedSubsystem {
+
+	private SpeedController motor;
+	private DigitalInput up;
+	private DigitalInput down;
+
+	public ballBlocker(SpeedController motor, DigitalInput down, DigitalInput up) {
+		this.up = up;
+		this.down = down;
+		this.motor = motor;
+	}
+
+	@Override
+	public boolean isMin() {
+		return down.get();
+	}
+
+	@Override
+	public boolean isMax() {
+		return down.get();
+	}
+
+	@Override
+	public PIDSource getPIDSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void move(double speed) {
+		motor.set(speed);
+	}
+}
+
