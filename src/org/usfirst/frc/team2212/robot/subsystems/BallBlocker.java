62bfe407f80d671840ce62e72bd11b7be051de19
Leon <Leon@192.168.1.110>
2017-02-08 18:09:41 +0200

    name changed

--- src/org/usfirst/frc/team2212/robot/subsystems/BallBlocker.java
+++ src/org/usfirst/frc/team2212/robot/subsystems/BallBlocker.java
@@ -1,22 +1,21 @@
 package org.usfirst.frc.team2212.robot.subsystems;
 
 import com.spikes2212.genericsubsystems.LimitedSubsystem;
-
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.command.Subsystem;
+
 
 /**
  *
  */
-public class ballBlocker extends LimitedSubsystem {
+public class BallBlocker extends LimitedSubsystem {
 
 	private SpeedController motor;
 	private DigitalInput up;
 	private DigitalInput down;
 
-	public ballBlocker(SpeedController motor, DigitalInput down, DigitalInput up) {
+	public BallBlocker(SpeedController motor, DigitalInput down, DigitalInput up) {
 		this.up = up;
 		this.down = down;
 		this.motor = motor;
