fbcb106e48c1f35a1469063280338e7cb0955dc6
Leon <Leon@LeonLaptop>
2017-02-09 18:12:10 +0200

    changed to CANTalon

--- src/org/usfirst/frc/team2212/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team2212/robot/subsystems/Shooter.java
@@ -1,25 +1,26 @@
 package org.usfirst.frc.team2212.robot.subsystems;
 
 import java.util.function.Supplier;
+
+import com.ctre.CANTalon;
 import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.genericsubsystems.LimitedSubsystem;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
-import edu.wpi.first.wpilibj.VictorSP;
 
 /**
  *
  */
 public class Shooter extends LimitedSubsystem {
 
-	private VictorSP motor;
+	private CANTalon motor;
 	private Encoder encoder;
 	public static final double DISTANCE_PER_PULSE = 1; // TODO check the real
 														// value
 	private double speed;
 
-	public Shooter(VictorSP motor, Encoder encoder) {
+	public Shooter(CANTalon motor, Encoder encoder) {
 		encoder.setPIDSourceType(PIDSourceType.kRate);
 		encoder.setDistancePerPulse(DISTANCE_PER_PULSE);
 		this.encoder = encoder;
