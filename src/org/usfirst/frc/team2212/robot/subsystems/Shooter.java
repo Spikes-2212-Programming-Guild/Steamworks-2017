f09cbaf9e3de1f9369b246e6fd9819f502dc120f
Leon <Leon@LeonLaptop>
2017-02-09 17:25:54 +0200

    now extends MoveLimitedSubsystemWithPID

--- src/org/usfirst/frc/team2212/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team2212/robot/subsystems/Shooter.java
@@ -1,29 +1,30 @@
 package org.usfirst.frc.team2212.robot.subsystems;
 
 import java.util.function.Supplier;
-
-import com.ctre.CANTalon;
 import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.genericsubsystems.LimitedSubsystem;
-
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.VictorSP;
 
 /**
  *
  */
 public class Shooter extends LimitedSubsystem {
 
-	private CANTalon motor;
+	private VictorSP motor;
 	private Encoder encoder;
-	public static final double DISTANCE_PER_PULSE = 1; //TODO check the real value
+	public static final double DISTANCE_PER_PULSE = 1; // TODO check the real
+														// value
+	private double speed;
 
-	public Shooter(CANTalon motor, Encoder encoder) {
+	public Shooter(VictorSP motor, Encoder encoder) {
 		this.encoder = encoder;
 		this.motor = motor;
 		encoder.setPIDSourceType(PIDSourceType.kRate);
 		encoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+		speed = 0;
 	}
 
 	@Override
@@ -42,7 +43,12 @@ public class Shooter extends LimitedSubsystem {
 	}
 
 	@Override
-	protected void move(double speed) {
+	protected void move(double additionalSpeed) {
+		speed += additionalSpeed * 0.01;
 		motor.set(speed);
+	}
+
+	public void stopShooting() {
+		motor.set(0);
 	}
 }
