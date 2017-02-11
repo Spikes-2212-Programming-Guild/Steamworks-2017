5d19672786de962fe53350b7ebf8af4ea2f60b7d
ThinkRedstone <ittai11@gmail.com>
2017-02-11 10:07:35 +0200

    Renamed OrienteToTwoObjects.java to TurnToTwoTargets.java

--- src/org/usfirst/frc/team2212/robot/commands/OrienteToTwoTargets.java
+++ src/org/usfirst/frc/team2212/robot/commands/OrienteToTwoTargets.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class OrienteToTwoTargets extends CommandGroup {
 
 	public OrienteToTwoTargets(Supplier<Double> rotateSpeedSupplier, double KP, double KI, double KD, double tolerance) {
-		addSequential(new OrienteToTwoObjects(rotateSpeedSupplier));
+		addSequential(new TurnToTwoTargets(rotateSpeedSupplier));
 		addSequential(new DriveTankWithPID(Robot.drivetrain, ImageProcessingConstants.leftSource,
 				ImageProcessingConstants.rightSource, 0, 0, KP, KI, KD, tolerance));
 	}
