cfb208defc97f9c13bde377f40a5982deec582a0
Tomer Sitton <tomer.sitton1@gmail.com>
2017-02-12 21:41:04 +0200

    deleted driveTrain

--- src/org/usfirst/frc/team2212/robot/commands/MoveStraightAndDropGearAuto.java
+++ src/org/usfirst/frc/team2212/robot/commands/MoveStraightAndDropGearAuto.java
@@ -22,8 +22,8 @@ public class MoveStraightAndDropGearAuto extends CommandGroup {
 	public static final Supplier<Double> movingTime = ConstantHandler
 			.addConstantDouble("MoveStraightAndDropGearAuto-movingTime", 3);
 
-	public MoveStraightAndDropGearAuto(TankDrivetrain drivetrain) {
-		addSequential(new DriveTank(drivetrain, movingSpeed, movingSpeed), movingTime.get());
+	public MoveStraightAndDropGearAuto() {
+		addSequential(new DriveTank(Robot.drivetrain, movingSpeed, movingSpeed), movingTime.get());
 		addSequential(new MoveLimitedSubsystem(Robot.gearDropper, GearDropper.OPEN_SPEED));
 	}
 }
