5de61efc85005dded78a2d34351c1f1b88c20ae7
Tomer Sitton <tomer.sitton1@gmail.com>
2017-02-12 17:55:02 +0200

    switched command from DropGear to MoveLimitedSubsystem

--- src/org/usfirst/frc/team2212/robot/commands/MoveStraightAndDropGearAuto.java
+++ src/org/usfirst/frc/team2212/robot/commands/MoveStraightAndDropGearAuto.java
@@ -2,7 +2,11 @@ package org.usfirst.frc.team2212.robot.commands;
 
 import java.util.function.Supplier;
 
+import org.usfirst.frc.team2212.robot.Robot;
+import org.usfirst.frc.team2212.robot.subsystems.GearDropper;
+
 import com.spikes2212.dashboard.ConstantHandler;
+import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
 import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTank;
 
@@ -13,12 +17,13 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class MoveStraightAndDropGearAuto extends CommandGroup {
 
-    public MoveStraightAndDropGearAuto(TankDrivetrain drivetrain) {
-    	
-    	Supplier<Double> movingSpeed = ConstantHandler.addConstantDouble("MoveStraightAndDropGearAuto-movingSpeed", 0.5);
-    	Supplier<Double> movingTime = ConstantHandler.addConstantDouble("MoveStraightAndDropGearAuto-movingTime", 3);
-    	
-        addSequential(new DriveTank(drivetrain, movingSpeed, movingSpeed), movingTime.get());
-        addSequential(new DropGear());
-    }
+	public MoveStraightAndDropGearAuto(TankDrivetrain drivetrain) {
+
+		Supplier<Double> movingSpeed = ConstantHandler.addConstantDouble("MoveStraightAndDropGearAuto-movingSpeed",
+				0.5);
+		Supplier<Double> movingTime = ConstantHandler.addConstantDouble("MoveStraightAndDropGearAuto-movingTime", 3);
+
+		addSequential(new DriveTank(drivetrain, movingSpeed, movingSpeed), movingTime.get());
+		addSequential(new MoveLimitedSubsystem(Robot.gearDropper, GearDropper.OPEN_SPEED));
+	}
 }
