92d3830272a019a277606d6305032a5747407f98
Tomer Sitton <tomer.sitton1@gmail.com>
2017-02-12 18:06:58 +0200

    added shooting angle

--- src/org/usfirst/frc/team2212/robot/Constants.java
+++ src/org/usfirst/frc/team2212/robot/Constants.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team2212.robot;
 
 public class Constants {
-	public static final double BOILER_HEIGHT = 2.74; // meter FIXME find the real number
+	public static final double BOILER_HEIGHT = 2.74; // meter FIXME find the
+														// real number
+	public static final double SHOOTING_ANGLE = 20; // FIXME find the real
+	// number
 }
