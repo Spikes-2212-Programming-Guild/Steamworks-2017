080bcf13985748f38584d3bc77925e4ab844ecef
Tomer Sitton <tomer.sitton1@gmail.com>
2017-02-12 21:33:31 +0200

    made constants to public static final

--- src/org/usfirst/frc/team2212/robot/commands/MoveDropGearMoveToBoilerAndShootAuto.java
+++ src/org/usfirst/frc/team2212/robot/commands/MoveDropGearMoveToBoilerAndShootAuto.java
@@ -19,27 +19,28 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class MoveDropGearMoveToBoilerAndShootAuto extends CommandGroup {
 
-	public MoveDropGearMoveToBoilerAndShootAuto(TankDrivetrain drivetrain) {
-		Supplier<Double> movingSpeed = ConstantHandler
-				.addConstantDouble("MoveDropGearMoveToBoilerAndShootAuto-movingSpeed", 0.5);
-		Supplier<Double> movingTime = ConstantHandler
-				.addConstantDouble("MoveDropGearMoveToBoilerAndShootAuto-movingTime", 3);
+	public static final Supplier<Double> movingSpeed = ConstantHandler
+			.addConstantDouble("MoveDropGearMoveToBoilerAndShootAuto-movingSpeed", 0.5);
+	public static final Supplier<Double> movingTime = ConstantHandler
+			.addConstantDouble("MoveDropGearMoveToBoilerAndShootAuto-movingTime", 3);
 
-		Supplier<Double> turnToGearSpeed = ConstantHandler
-				.addConstantDouble("MoveDropGearMoveToBoilerAndShootAuto-turnToGearSpeed", 3);
+	public static final Supplier<Double> turnToGearSpeed = ConstantHandler
+			.addConstantDouble("MoveDropGearMoveToBoilerAndShootAuto-turnToGearSpeed", 3);
 
-		Supplier<Double> turnToBoilerSpeed = ConstantHandler
-				.addConstantDouble("MoveDropGearMoveToBoilerAndShootAuto-turnToBoilerSpeed", 0.5);
-		Supplier<Double> moveToBoilerSpeed = ConstantHandler
-				.addConstantDouble("DropGearMoveToBoilerAndShootAuto-moveToBoilerSpeedMove", 0.5);
-		Supplier<Double> moveToBoilerTime = ConstantHandler
-				.addConstantDouble("MoveDropGearMoveToBoilerAndShootAuto-moveToBoilerTime", 3);
+	public static final Supplier<Double> turnToBoilerSpeed = ConstantHandler
+			.addConstantDouble("MoveDropGearMoveToBoilerAndShootAuto-turnToBoilerSpeed", 0.5);
+	public static final Supplier<Double> moveToBoilerSpeed = ConstantHandler
+			.addConstantDouble("DropGearMoveToBoilerAndShootAuto-moveToBoilerSpeedMove", 0.5);
+	public static final Supplier<Double> moveToBoilerTime = ConstantHandler
+			.addConstantDouble("MoveDropGearMoveToBoilerAndShootAuto-moveToBoilerTime", 3);
 
-		addSequential(new DriveTank(drivetrain, movingSpeed, movingSpeed), movingTime.get());
+	public MoveDropGearMoveToBoilerAndShootAuto() {
+
+		addSequential(new DriveTank(Robot.drivetrain, movingSpeed, movingSpeed), movingTime.get());
 		addSequential(new OrientToGear(turnToGearSpeed));
 		addSequential(new MoveLimitedSubsystem(Robot.gearDropper, GearDropper.OPEN_SPEED));
 
-		addSequential(new DriveTank(drivetrain, moveToBoilerSpeed, moveToBoilerSpeed), moveToBoilerTime.get());
+		addSequential(new DriveTank(Robot.drivetrain, moveToBoilerSpeed, moveToBoilerSpeed), moveToBoilerTime.get());
 		addSequential(new OrientToBoiler(turnToBoilerSpeed));
 		addSequential(new ShootByDistance(Robot.shooter, ImageProcessingConstants.distance, Shooter.KP, Shooter.KI,
 				Shooter.KD));
