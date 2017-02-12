634dd9d8cec2c45190d8f7c24b64fcd05fb7133c
Tomer Sitton <tomer.sitton1@gmail.com>
2017-02-12 17:55:40 +0200

    switched command from DropGear to MoveLimitedSubsystem

--- src/org/usfirst/frc/team2212/robot/commands/MoveTurnLeftAndDropGearAuto.java
+++ src/org/usfirst/frc/team2212/robot/commands/MoveTurnLeftAndDropGearAuto.java
@@ -2,7 +2,11 @@ package org.usfirst.frc.team2212.robot.commands;
 
 import java.util.function.Supplier;
 
+import org.usfirst.frc.team2212.robot.Robot;
+import org.usfirst.frc.team2212.robot.subsystems.GearDropper;
+
 import com.spikes2212.dashboard.ConstantHandler;
+import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
 import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTank;
 
@@ -13,15 +17,18 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class MoveTurnLeftAndDropGearAuto extends CommandGroup {
 
-	 public MoveTurnLeftAndDropGearAuto(TankDrivetrain drivetrain) {
-	    	Supplier<Double> movingSpeed = ConstantHandler.addConstantDouble("MoveTurnLeftAndDropGearAuto-movingSpeed", -0.5);
-	    	Supplier<Double> turningSpeed = ConstantHandler.addConstantDouble("MoveTurnLeftAndDropGearAuto-turningSpeed", -0.5);
-	    	Supplier<Double> moveToGearTime = ConstantHandler.addConstantDouble("MoveTurnLeftAndDropGearAuto-moveToGearTime", 1);
-	    	Supplier<Double> movingTime = ConstantHandler.addConstantDouble("MoveTurnLeftAndDropGearAuto-movingTime", 4);
-	      
-	    	addSequential(new DriveTank(drivetrain, movingSpeed, movingSpeed), movingTime.get());
-	        addSequential(new OrientToGear(turningSpeed));
-	        addSequential(new DriveTank(drivetrain, movingSpeed, movingSpeed), moveToGearTime.get());
-	        addSequential(new DropGear());
-	    }
+	public MoveTurnLeftAndDropGearAuto(TankDrivetrain drivetrain) {
+		Supplier<Double> movingSpeed = ConstantHandler.addConstantDouble("MoveTurnLeftAndDropGearAuto-movingSpeed",
+				-0.5);
+		Supplier<Double> turningSpeed = ConstantHandler.addConstantDouble("MoveTurnLeftAndDropGearAuto-turningSpeed",
+				-0.5);
+		Supplier<Double> moveToGearTime = ConstantHandler
+				.addConstantDouble("MoveTurnLeftAndDropGearAuto-moveToGearTime", 1);
+		Supplier<Double> movingTime = ConstantHandler.addConstantDouble("MoveTurnLeftAndDropGearAuto-movingTime", 4);
+
+		addSequential(new DriveTank(drivetrain, movingSpeed, movingSpeed), movingTime.get());
+		addSequential(new OrientToGear(turningSpeed));
+		addSequential(new DriveTank(drivetrain, movingSpeed, movingSpeed), moveToGearTime.get());
+		addSequential(new MoveLimitedSubsystem(Robot.gearDropper, GearDropper.OPEN_SPEED));
+	}
 }
