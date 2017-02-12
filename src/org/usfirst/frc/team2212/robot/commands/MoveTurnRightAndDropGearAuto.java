e5ad3873e922240d53f1035c3ed684429cbff38b
Tomer Sitton <tomer.sitton1@gmail.com>
2017-02-12 17:56:16 +0200

    switched command from DropGear to MoveLimitedSubsystem

--- src/org/usfirst/frc/team2212/robot/commands/MoveTurnRightAndDropGearAuto.java
+++ src/org/usfirst/frc/team2212/robot/commands/MoveTurnRightAndDropGearAuto.java
@@ -2,7 +2,11 @@ package org.usfirst.frc.team2212.robot.commands;
 
 import java.util.function.Supplier;
 
+import org.usfirst.frc.team2212.robot.Robot;
+import org.usfirst.frc.team2212.robot.subsystems.GearDropper;
+
 import com.spikes2212.dashboard.ConstantHandler;
+import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
 import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTank;
 
@@ -14,14 +18,17 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class MoveTurnRightAndDropGearAuto extends CommandGroup {
 
 	public MoveTurnRightAndDropGearAuto(TankDrivetrain drivetrain) {
-		Supplier<Double> movingSpeed = ConstantHandler.addConstantDouble("MoveTurnRightAndDropGearAuto-movingSpeed", 0.5);
-		Supplier<Double> turningSpeed = ConstantHandler.addConstantDouble("MoveTurnRightAndDropGearAuto-turningSpeed", 0.5);
-		Supplier<Double> moveToGearTime = ConstantHandler.addConstantDouble("MoveTurnRightAndDropGearAuto-moveToGearTime", 1);
+		Supplier<Double> movingSpeed = ConstantHandler.addConstantDouble("MoveTurnRightAndDropGearAuto-movingSpeed",
+				0.5);
+		Supplier<Double> turningSpeed = ConstantHandler.addConstantDouble("MoveTurnRightAndDropGearAuto-turningSpeed",
+				0.5);
+		Supplier<Double> moveToGearTime = ConstantHandler
+				.addConstantDouble("MoveTurnRightAndDropGearAuto-moveToGearTime", 1);
 		Supplier<Double> movingTime = ConstantHandler.addConstantDouble("MoveTurnRightAndDropGearAuto-movingTime", 4);
-		
+
 		addSequential(new DriveTank(drivetrain, movingSpeed, movingSpeed), movingTime.get());
 		addSequential(new OrientToGear(turningSpeed));
 		addSequential(new DriveTank(drivetrain, movingSpeed, movingSpeed), moveToGearTime.get());
-		addSequential(new DropGear());
+		addSequential(new MoveLimitedSubsystem(Robot.gearDropper, GearDropper.OPEN_SPEED));
 	}
 }
