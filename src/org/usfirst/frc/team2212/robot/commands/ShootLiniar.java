681f20d986028e1991e163fcbec9529e7166808c
omri cohen <omrichoen@gmail.com>
2017-02-14 18:31:34 +0200

    rename the constant

--- src/org/usfirst/frc/team2212/robot/commands/ShootLiniar.java
+++ src/org/usfirst/frc/team2212/robot/commands/ShootLiniar.java
@@ -8,14 +8,14 @@ import com.spikes2212.dashboard.ConstantHandler;
 
 public class ShootLiniar extends ShootBySpeed {
 
-	public static final Supplier<Double> generalConstant = ConstantHandler.addConstantDouble("ShootLiniar-generalConstant", 0.7);
+	public static final Supplier<Double> distanceToSpeedRatio = ConstantHandler.addConstantDouble("ShootLiniar-distanceToSpeedRatio", 0.7);
 
 	public ShootLiniar(Shooter shooter, Supplier<Double> distance, double KP, double KI, double KD) {
 		super(shooter, () -> calculateSpeedByDistance(distance.get()), KP, KI, KD);
 	}
 
 	public static double calculateSpeedByDistance(double distance) {
-		double speed = distance * generalConstant.get();
+		double speed = distance * distanceToSpeedRatio.get();
 		return speed;
 	}
 }
