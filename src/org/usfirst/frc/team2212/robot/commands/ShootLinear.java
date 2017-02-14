c3a217d02ba039aeefd7456db7a4ab67fed4fd00
omri cohen <omrichoen@gmail.com>
2017-02-14 18:44:27 +0200

    renamed

--- src/org/usfirst/frc/team2212/robot/commands/ShootLinear.java
+++ src/org/usfirst/frc/team2212/robot/commands/ShootLinear.java
@@ -6,11 +6,11 @@ import org.usfirst.frc.team2212.robot.subsystems.Shooter;
 
 import com.spikes2212.dashboard.ConstantHandler;
 
-public class ShootLiniar extends ShootBySpeed {
+public class ShootLinear extends ShootBySpeed {
 
-	public static final Supplier<Double> distanceToSpeedRatio = ConstantHandler.addConstantDouble("ShootLiniar-distanceToSpeedRatio", 0.7);
+	public static final Supplier<Double> distanceToSpeedRatio = ConstantHandler.addConstantDouble("ShootLinear-distanceToSpeedRatio", 0.7);
 
-	public ShootLiniar(Shooter shooter, Supplier<Double> distance, double KP, double KI, double KD) {
+	public ShootLinear(Shooter shooter, Supplier<Double> distance, double KP, double KI, double KD) {
 		super(shooter, () -> calculateSpeedByDistance(distance.get()), KP, KI, KD);
 	}
 
