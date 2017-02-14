a1e18aa872747a95ce94efae72eef5a62303f672
omri cohen <omrichoen@gmail.com>
2017-02-14 21:43:36 +0200

    uses driveArcade

--- src/org/usfirst/frc/team2212/robot/commands/OrientateAndMoveToGear.java
+++ src/org/usfirst/frc/team2212/robot/commands/OrientateAndMoveToGear.java
@@ -5,6 +5,7 @@ import java.util.function.Supplier;
 import org.usfirst.frc.team2212.robot.Robot;
 
 import com.spikes2212.dashboard.ConstantHandler;
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcade;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTank;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -16,6 +17,6 @@ public class OrientateAndMoveToGear extends CommandGroup {
 
 	public OrientateAndMoveToGear(Supplier<Double> turningSpeed, Supplier<Double> forwardsSpeed) {
 		addSequential(new OrientToGear(turningSpeed));
-		addSequential(new DriveTank(Robot.drivetrain, forwardsSpeed, forwardsSpeed));
+		addSequential(new DriveArcade(Robot.drivetrain, forwardsSpeed.get(), 0));
 	}
 }
