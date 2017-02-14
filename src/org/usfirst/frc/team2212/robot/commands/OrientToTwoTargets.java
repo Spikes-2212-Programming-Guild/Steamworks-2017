1acbce9a9f59d5869c63a78895ebb02c7d480e6d
Spikes 2212 <spikes.no2212@gmail.com>
2017-02-14 20:08:11 +0200

    added PIDsource to OrientateToTwoTargets

--- src/org/usfirst/frc/team2212/robot/commands/OrientToTwoTargets.java
+++ src/org/usfirst/frc/team2212/robot/commands/OrientToTwoTargets.java
@@ -7,13 +7,47 @@ import org.usfirst.frc.team2212.robot.Robot;
 
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTankWithPID;
 
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class OrientToTwoTargets extends CommandGroup {
+	public static PIDSource leftSource = new PIDSource() {
+
+		@Override
+		public void setPIDSourceType(PIDSourceType pidSource) {
+		}
+
+		@Override
+		public double pidGet() {
+			return -ImageProcessingConstants.TWO_OBJECTS_CENTER.get();
+		}
+
+		@Override
+		public PIDSourceType getPIDSourceType() {
+			return PIDSourceType.kDisplacement;
+		}
+	};
+	public static PIDSource rightSource = new PIDSource() {
+
+		@Override
+		public void setPIDSourceType(PIDSourceType pidSource) {
+		}
+
+		@Override
+		public double pidGet() {
+			return ImageProcessingConstants.TWO_OBJECTS_CENTER.get();
+		}
+
+		@Override
+		public PIDSourceType getPIDSourceType() {
+			return PIDSourceType.kDisplacement;
+		}
+	};
 
 	public OrientToTwoTargets(Supplier<Double> rotateSpeedSupplier, double KP, double KI, double KD, double tolerance) {
 		addSequential(new TurnToTwoTargets(rotateSpeedSupplier));
-		addSequential(new DriveTankWithPID(Robot.drivetrain, ImageProcessingConstants.leftSource,
-				ImageProcessingConstants.rightSource, 0, 0, KP, KI, KD, tolerance));
+		addSequential(new DriveTankWithPID(Robot.drivetrain, leftSource,
+				rightSource, 0, 0, KP, KI, KD, tolerance));
 	}
 }
