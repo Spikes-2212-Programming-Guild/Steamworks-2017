1f543f9415ed5bab7f46056dda83dd7920dafdfc
Tomer Sitton <tomer.sitton1@gmail.com>
2017-02-15 18:26:49 +0200

    made it working

--- src/org/usfirst/frc/team2212/robot/commands/ShootBySpeed.java
+++ src/org/usfirst/frc/team2212/robot/commands/ShootBySpeed.java
@@ -2,27 +2,23 @@ package org.usfirst.frc.team2212.robot.commands;
 
 import java.util.function.Supplier;
 
+import org.usfirst.frc.team2212.robot.Robot;
 import org.usfirst.frc.team2212.robot.subsystems.Shooter;
 
 import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystemWithPID;
-
-import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.PIDSource;
-import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class ShootBySpeed extends MoveLimitedSubsystemWithPID {
-	private Shooter shooter;
 
-	public ShootBySpeed(Shooter shooter, Supplier<Double> wantedSpeed, double KP, double KI, double KD) {
-		this(shooter, wantedSpeed.get(), KP, KI, KD);
+	public ShootBySpeed(Supplier<Double> wantedSpeed) {
+		this(wantedSpeed.get());
 	}
 
-	public ShootBySpeed(Shooter shooter, double wantedSpeed, double KP, double KI, double KD) {
-		super(shooter, wantedSpeed, KP, KI, KD, 0);
-		this.shooter = shooter;
+	public ShootBySpeed(double wantedSpeed) {
+		super(Robot.shooter, Robot.shooter.getPIDSource(), wantedSpeed, Shooter.KP.get(), Shooter.KI.get(),
+				Shooter.KD.get(), 0);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
