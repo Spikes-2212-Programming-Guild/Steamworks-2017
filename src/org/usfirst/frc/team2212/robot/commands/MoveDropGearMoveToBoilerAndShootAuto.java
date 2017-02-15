64ea69ecba51002dafff9bda8956bc2039c20b22
Tomer Sitton <tomer.sitton1@gmail.com>
2017-02-15 17:44:08 +0200

    removed unneeded imports

--- src/org/usfirst/frc/team2212/robot/commands/MoveDropGearMoveToBoilerAndShootAuto.java
+++ src/org/usfirst/frc/team2212/robot/commands/MoveDropGearMoveToBoilerAndShootAuto.java
@@ -4,12 +4,9 @@ import java.util.function.Supplier;
 
 import org.usfirst.frc.team2212.robot.ImageProcessingConstants;
 import org.usfirst.frc.team2212.robot.Robot;
-import org.usfirst.frc.team2212.robot.subsystems.GearDropper;
 import org.usfirst.frc.team2212.robot.subsystems.Shooter;
 
 import com.spikes2212.dashboard.ConstantHandler;
-import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
-import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTank;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -36,9 +33,10 @@ public class MoveDropGearMoveToBoilerAndShootAuto extends CommandGroup {
 
 	public MoveDropGearMoveToBoilerAndShootAuto() {
 
-		addSequential(new MoveTurnLeftAndDropGearAuto()); //Moves Turns Left And Drops Gear
+		addSequential(new MoveTurnLeftAndDropGearAuto()); // Moves Turns Left
+															// And Drops Gear
 
-		//moves to the boiler turns to it and shoots
+		// moves to the boiler turns to it and shoots
 		addSequential(new DriveTank(Robot.drivetrain, moveToBoilerSpeed, moveToBoilerSpeed), moveToBoilerTime.get());
 		addSequential(new OrientToBoiler(turnToBoilerSpeed));
 		addSequential(new ShootByDistance(Robot.shooter, ImageProcessingConstants.distanceToBoiler, Shooter.KP.get(),
