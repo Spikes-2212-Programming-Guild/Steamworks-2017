b14f7c6ad9386b61e31872a0c3bae0a7c17461b7
Tomer Sitton <tomer.sitton1@gmail.com>
2017-02-15 19:15:06 +0200

    moved PID constants from OrientToGear.java

--- src/org/usfirst/frc/team2212/robot/commands/OrientToGear.java
+++ src/org/usfirst/frc/team2212/robot/commands/OrientToGear.java
@@ -11,17 +11,15 @@ import org.usfirst.frc.team2212.robot.ImageProcessingConstants;
  *
  */
 public class OrientToGear extends CommandGroup {
+
 	public static final Supplier<Double> GEAR_CAMERA_ID = ConstantHandler.addConstantDouble("OrientToGear-CAMERA_ID",
 			0);
-	public static final Supplier<Double> KP = ConstantHandler.addConstantDouble("OrientToGear-KP", 0);
-	public static final Supplier<Double> KI = ConstantHandler.addConstantDouble("OrientToGear-KI", 0);
-	public static final Supplier<Double> KD = ConstantHandler.addConstantDouble("OrientToGear-KD", 0);
-	public static final Supplier<Double> TOLERANCE = ConstantHandler.addConstantDouble("OrientToGear-TOLERANCE", 0);
 
 	public OrientToGear(Supplier<Double> turningSpeed) {
 		addSequential(new RunnableCommand(
 				() -> ImageProcessingConstants.NETWORK_TABLE.putNumber("currentCamera", GEAR_CAMERA_ID.get())));
-		addSequential(new OrientToTwoTargets(turningSpeed, KP.get(), KI.get(), KD.get(), TOLERANCE.get()));
+		addSequential(new OrientToTwoTargets(turningSpeed, OrientToTwoTargets.KP.get(), OrientToTwoTargets.KI.get(),
+				OrientToTwoTargets.KD.get(), OrientToTwoTargets.TOLERANCE.get()));
 	}
 
 }
