00c466339d3f0e988251a0fb5963fe18aba1300a
Tomer Sitton <tomer.sitton1@gmail.com>
2017-02-15 17:48:25 +0200

    removed unneeded imports

--- src/org/usfirst/frc/team2212/robot/commands/MoveTurnLeftAndDropGearAuto.java
+++ src/org/usfirst/frc/team2212/robot/commands/MoveTurnLeftAndDropGearAuto.java
@@ -7,7 +7,6 @@ import org.usfirst.frc.team2212.robot.subsystems.GearDropper;
 
 import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
-import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTank;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -19,14 +18,14 @@ public class MoveTurnLeftAndDropGearAuto extends CommandGroup {
 
 	public static final Supplier<Double> turningSpeed = ConstantHandler
 			.addConstantDouble("MoveTurnLeftAndDropGearAuto-turningSpeed", -0.5);
-	public static final Supplier<Double> movingTime = ConstantHandler
+	public static final Supplier<Double> movingStraightTime = ConstantHandler
 			.addConstantDouble("MoveTurnLeftAndDropGearAuto-movingTime", 4);
 	public static final Supplier<Double> moveToGearTime = ConstantHandler
 			.addConstantDouble("MoveTurnLeftAndDropGearAuto-moveToGearTime", 1);
 
 	public MoveTurnLeftAndDropGearAuto() {
 		addSequential(new DriveTank(Robot.drivetrain, MoveStraightAndDropGearAuto.movingSpeed,
-				MoveStraightAndDropGearAuto.movingSpeed), movingTime.get());
+				MoveStraightAndDropGearAuto.movingSpeed), movingStraightTime.get());
 		addSequential(new OrientateAndMoveToGear(turningSpeed, MoveStraightAndDropGearAuto.movingSpeed),
 				moveToGearTime.get());
 		addSequential(new MoveLimitedSubsystem(Robot.gearDropper, GearDropper.OPEN_SPEED));
