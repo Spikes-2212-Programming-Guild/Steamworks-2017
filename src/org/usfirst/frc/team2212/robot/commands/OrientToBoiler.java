7148328f71fb04fc678fb77d2f9cb432380f6714
Tomer Sitton <tomer.sitton1@gmail.com>
2017-02-15 19:17:39 +0200

    fixed calling to OrientToTwoTargets

--- src/org/usfirst/frc/team2212/robot/commands/OrientToBoiler.java
+++ src/org/usfirst/frc/team2212/robot/commands/OrientToBoiler.java
@@ -11,10 +11,13 @@ import org.usfirst.frc.team2212.robot.ImageProcessingConstants;
  *
  */
 public class OrientToBoiler extends CommandGroup {
-    public static final Supplier<Double> BOILER_CAMERA_ID = ConstantHandler.addConstantDouble("OrientToBoiler-CAMERA_ID", 0);
+	public static final Supplier<Double> BOILER_CAMERA_ID = ConstantHandler
+			.addConstantDouble("OrientToBoiler-CAMERA_ID", 0);
 
-    public OrientToBoiler(Supplier<Double> turningSpeed) {
-        addSequential(new RunnableCommand(() -> ImageProcessingConstants.NETWORK_TABLE.putNumber("currentCamera", BOILER_CAMERA_ID.get())));
-        addSequential(new OrientToTwoTargets(turningSpeed, OrientToGear.KP.get(), OrientToGear.KI.get(), OrientToGear.KD.get(), OrientToGear.TOLERANCE.get()));
-    }
+	public OrientToBoiler(Supplier<Double> turningSpeed) {
+		addSequential(new RunnableCommand(
+				() -> ImageProcessingConstants.NETWORK_TABLE.putNumber("currentCamera", BOILER_CAMERA_ID.get())));
+		addSequential(new OrientToTwoTargets(turningSpeed, OrientToTwoTargets.KP.get(), OrientToTwoTargets.KI.get(),
+				OrientToTwoTargets.KD.get(), OrientToTwoTargets.TOLERANCE.get()));
+	}
 }
