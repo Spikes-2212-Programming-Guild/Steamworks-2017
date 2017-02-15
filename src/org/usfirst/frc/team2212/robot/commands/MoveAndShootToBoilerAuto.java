a43f2254e87771ca73d704bea4b799a31139b46c
Tomer Sitton <tomer.sitton1@gmail.com>
2017-02-15 17:43:41 +0200

    removed unneeded imports

--- src/org/usfirst/frc/team2212/robot/commands/MoveAndShootToBoilerAuto.java
+++ src/org/usfirst/frc/team2212/robot/commands/MoveAndShootToBoilerAuto.java
@@ -7,7 +7,6 @@ import org.usfirst.frc.team2212.robot.Robot;
 import org.usfirst.frc.team2212.robot.subsystems.Shooter;
 
 import com.spikes2212.dashboard.ConstantHandler;
-import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTank;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -16,6 +15,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 public class MoveAndShootToBoilerAuto extends CommandGroup {
+
 	public static final Supplier<Double> movingSpeed = ConstantHandler
 			.addConstantDouble("MoveAndShootToBoiler-movingSpeed", 0.5);
 	public static final Supplier<Double> turningSpeed = ConstantHandler
