2cd9a4d839f024049aa6efd677dad25dda9f5d53
omri cohen <omrichoen@gmail.com>
2017-02-16 21:50:26 +0200

    pressed ctrl shift f like a boss

--- src/org/usfirst/frc/team2212/robot/OI.java
+++ src/org/usfirst/frc/team2212/robot/OI.java
@@ -16,22 +16,22 @@ import com.spikes2212.utils.XboXUID;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI /* GEVALD */ {
-	
+
 	// TODO CHANGE ALL SPEEDS!
 
 	// controllers
 	private Joystick driverRight = new Joystick(0);
 	private Joystick driverLeft = new Joystick(1);
-	
+
 	private Joystick navigatorJoystick = new Joystick(2);
 	private XboXUID navigatorXbox = new XboXUID(3);
-	
-	// driverRight buttons TODO maybe there are more buttons 
+
+	// driverRight buttons TODO maybe there are more buttons
 	private Button switchToFrontCameraButton;
 	private Button switchToRearCameraButton;
 	private Button orientateAndMoveToGearButton;
 	private Button orientToBoiler;
-	
+
 	// joystick navigator buttons
 	private Button dropGearButton;
 	private Button raiseBallBlockerButton;
@@ -39,7 +39,7 @@ public class OI /* GEVALD */ {
 	private Button shootFuelButton;
 	private Button pickFuelButton;
 	private Button climbRopeButton;
-	
+
 	// Xbox navigator buttons
 	private Button dropGearXbox;
 	private Button raiseBallBlockerXbox;
@@ -47,7 +47,7 @@ public class OI /* GEVALD */ {
 	private Button shootFuelXbox;
 	private Button pickFuelXbox;
 	private Button climbRopeXbox;
-	
+
 	// constructor
 	public OI() {
 		initJoystickDriver();
@@ -59,22 +59,22 @@ public class OI /* GEVALD */ {
 	private void initJoystickDriver() {
 		switchToFrontCameraButton = new JoystickButton(driverRight, 3);
 		switchToRearCameraButton = new JoystickButton(driverRight, 4);
-		orientateAndMoveToGearButton= new JoystickButton(driverRight, 0);
+		orientateAndMoveToGearButton = new JoystickButton(driverRight, 0);
 		orientToBoiler = new JoystickButton(driverRight, 1);
-		
-		switchToFrontCameraButton.whenPressed(new RunnableCommand(()->Robot.camerasHandler.switchCamera(1)));
-		switchToRearCameraButton.whenPressed(new RunnableCommand(()->Robot.camerasHandler.switchCamera(0)));
+
+		switchToFrontCameraButton.whenPressed(new RunnableCommand(() -> Robot.camerasHandler.switchCamera(1)));
+		switchToRearCameraButton.whenPressed(new RunnableCommand(() -> Robot.camerasHandler.switchCamera(0)));
 	}
-	
+
 	// sets all commands and buttons connected to navigatorJoystick
-	private void initJoystickNavigator(){
+	private void initJoystickNavigator() {
 		dropGearButton = new JoystickButton(navigatorJoystick, 0);
 		raiseBallBlockerButton = new JoystickButton(navigatorJoystick, 1);
 		lowerBallBlockerButton = new JoystickButton(navigatorJoystick, 2);
 		shootFuelButton = new JoystickButton(navigatorJoystick, 3);
 		pickFuelButton = new JoystickButton(navigatorJoystick, 4);
 		climbRopeButton = new JoystickButton(navigatorJoystick, 5);
-		
+
 		dropGearButton.whenPressed(new MoveLimitedSubsystem(Robot.gearDropper, 0.7));
 		raiseBallBlockerButton.whileHeld(new MoveLimitedSubsystem(Robot.ballBlocker, 0.7));
 		lowerBallBlockerButton.whileHeld(new MoveLimitedSubsystem(Robot.ballBlocker, -0.7));
@@ -84,14 +84,14 @@ public class OI /* GEVALD */ {
 	}
 
 	// sets all commands connected to navigatorXbox
-	private void initXboxNavigator(){
+	private void initXboxNavigator() {
 		dropGearXbox = navigatorXbox.getBlueButton();
 		raiseBallBlockerXbox = navigatorXbox.getUpButton();
 		lowerBallBlockerXbox = navigatorXbox.getDownButton();
 		shootFuelXbox = navigatorXbox.getRbButton();
 		pickFuelXbox = navigatorXbox.getYellowButton();
 		climbRopeXbox = navigatorXbox.getGreenButton();
-		
+
 		dropGearXbox.whenPressed(new MoveLimitedSubsystem(Robot.gearDropper, 0.7));
 		raiseBallBlockerXbox.whileHeld(new MoveLimitedSubsystem(Robot.ballBlocker, 0.7));
 		lowerBallBlockerXbox.whileHeld(new MoveLimitedSubsystem(Robot.ballBlocker, -0.7));
@@ -99,25 +99,25 @@ public class OI /* GEVALD */ {
 		pickFuelXbox.whileHeld(new MoveLimitedSubsystem(Robot.picker, 0.7));
 		climbRopeXbox.whenPressed(new MoveLimitedSubsystem(Robot.climber, 0.7));
 	}
-	
-	//receives input, returns the adjusted input for better sensitivity
+
+	// receives input, returns the adjusted input for better sensitivity
 	private double adjustInput(double input) {
 		return input * Math.abs(input);
 	}
-	
-	//returns the adjusted value of the Rotate 
+
+	// returns the adjusted value of the Rotate
 	// switch this to switch between the 2 drive arcade methods
-	public double getRotate(){
+	public double getRotate() {
 		return adjustInput(driverLeft.getX());
 	}
-		
-	//returns the adjusted value of the driving right joystick's y
-	public double getForwardRight(){
+
+	// returns the adjusted value of the driving right joystick's y
+	public double getForwardRight() {
 		return adjustInput(driverRight.getY());
 	}
-	
-	//returns the adjusted value of the driving left joystick's y
-	public double getForwardLeft(){
+
+	// returns the adjusted value of the driving left joystick's y
+	public double getForwardLeft() {
 		return adjustInput(driverLeft.getY());
 	}
 }
