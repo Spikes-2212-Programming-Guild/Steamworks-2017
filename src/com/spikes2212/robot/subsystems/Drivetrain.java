573f1e6cefc3b3120f1da9d48ffedf0628007d50
ThinkRedstone <ittai11@gmail.com>
2017-02-19 18:35:09 +0200

    Revert "Uncommited testing code from Drivetrain.java"
    This reverts commit e611aab1c083a970c49f5c198ae975aaf6f798a0.

--- src/com/spikes2212/robot/subsystems/Drivetrain.java
+++ src/com/spikes2212/robot/subsystems/Drivetrain.java
@@ -1,9 +1,12 @@
 package com.spikes2212.robot.subsystems;
 
 import com.spikes2212.genericsubsystems.drivetrains.TankDrivetrain;
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcade;
+import com.spikes2212.robot.Robot;
 import com.spikes2212.utils.DoubleSpeedcontroller;
 
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -13,44 +16,51 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Drivetrain extends TankDrivetrain {
 
-    public static final double DISTANCE_PER_PULSE = 4 * Math.PI / 1440; // 1440 pulses per revolution,4 inch wheel
-    private SpeedController leftSpeedcontroller;
-    private SpeedController rightSpeedcontroller;
-    private Encoder leftEncoder;
-    private Encoder rightEncoder;
+	public static final double DISTANCE_PER_PULSE = 4 * Math.PI / 1440; // 1440
+																		// pulses
+																		// per
+																		// revolution,4
+																		// inch
+																		// wheel
+	private SpeedController leftSpeedcontroller;
+	private SpeedController rightSpeedcontroller;
+	private Encoder leftEncoder;
+	private Encoder rightEncoder;
+	private Joystick j = new Joystick(0);
 
-    public Drivetrain(SpeedController leftSpeedcontroller, SpeedController rightSpeedcontroller, Encoder leftEncoder,
-                      Encoder rightEncoder) {
-        this.leftSpeedcontroller = leftSpeedcontroller;
-        this.rightSpeedcontroller = rightSpeedcontroller;
-        this.rightSpeedcontroller.setInverted(true);
-        this.leftEncoder = leftEncoder;
-        this.rightEncoder = rightEncoder;
-        leftEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
-        rightEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
-    }
+	public Drivetrain(SpeedController leftSpeedcontroller, SpeedController rightSpeedcontroller, Encoder leftEncoder,
+			Encoder rightEncoder) {
+		this.leftSpeedcontroller = leftSpeedcontroller;
+		this.rightSpeedcontroller = rightSpeedcontroller;
+		this.rightSpeedcontroller.setInverted(true);
+		this.leftEncoder = leftEncoder;
+		this.rightEncoder = rightEncoder;
+		leftEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+		rightEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+	}
 
-    @Override
-    public void setLeft(double speedLeft) {
-        leftSpeedcontroller.set(speedLeft);
-    }
+	@Override
+	public void setLeft(double speedLeft) {
+		leftSpeedcontroller.set(speedLeft);
+	}
 
-    @Override
-    public void setRight(double speedRight) {
-        rightSpeedcontroller.set(speedRight);
-    }
+	@Override
+	public void setRight(double speedRight) {
+		rightSpeedcontroller.set(speedRight);
+	}
 
-    @Override
-    public PIDSource getLeftPIDSource() {
-        return leftEncoder;
-    }
+	@Override
+	public PIDSource getLeftPIDSource() {
+		return leftEncoder;
+	}
 
-    @Override
-    public PIDSource getRightPIDSource() {
-        return rightEncoder;
-    }
+	@Override
+	public PIDSource getRightPIDSource() {
+		return rightEncoder;
+	}
 
-    @Override
-    protected void initDefaultCommand() {
-    }
+	@Override
+	protected void initDefaultCommand() {
+		setDefaultCommand(new DriveArcade(this, () -> -j.getY(), () -> -j.getX()));
+	}
 }
