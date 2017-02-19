151d5d20b421112bded3cb98853eabea42eb75ad
ThinkRedstone <ittai11@gmail.com>
2017-02-19 18:30:29 +0200

    Revert "Uncommited testing code from OI.java"
    This reverts commit 512d74007eccb2391b28f3587a1c2ac93bf42492.

--- src/com/spikes2212/robot/OI.java
+++ src/com/spikes2212/robot/OI.java
@@ -1,37 +1,55 @@
 package com.spikes2212.robot;
 
-import edu.wpi.first.wpilibj.buttons.Button;
+import com.spikes2212.dashboard.ConstantHandler;
+import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTank;
+import com.spikes2212.robot.commands.orientation.OrientToBoiler;
+import com.spikes2212.robot.commands.orientation.OrientToGear;
+import com.spikes2212.robot.commands.orientation.OrientToTwoTargets;
+import com.spikes2212.robot.commands.shooting.ShootByDistance;
+import com.spikes2212.robot.commands.shooting.ShootBySpeed;
+import com.spikes2212.robot.commands.shooting.ShootLinear;
+import com.spikes2212.robot.subsystems.BallBlocker;
+import com.spikes2212.robot.subsystems.Climber;
+import com.spikes2212.robot.subsystems.Feeder;
+import com.spikes2212.robot.subsystems.GearDropper;
+import com.spikes2212.robot.subsystems.Picker;
+import com.spikes2212.utils.RunnableCommand;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
-public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
+public class OI /* GEVALD */ {
+	public OI() {
+		SmartDashboard.putData("move-BallBlocker-up",
+				new MoveLimitedSubsystem(Robot.ballBlocker, BallBlocker.UP_SPEED));
+		SmartDashboard.putData("move-BallBlocker-down",
+				new MoveLimitedSubsystem(Robot.ballBlocker, BallBlocker.DOWN_SPEED));
+		SmartDashboard.putData("open-GearDropper",
+				new MoveLimitedSubsystem(Robot.gearDropper, GearDropper.OPENING_SPEED));
+		SmartDashboard.putData("close-GearDropper",
+				new MoveLimitedSubsystem(Robot.gearDropper, GearDropper.CLOSING_SPEED));
+		SmartDashboard.putData("Climb", new MoveLimitedSubsystem(Robot.climber, Climber.SPEED));
+		SmartDashboard.putData("Feed", new MoveLimitedSubsystem(Robot.feeder, Feeder.SPEED));
+		SmartDashboard.putData("Pick", new MoveLimitedSubsystem(Robot.picker, Picker.SPEED));
+		SmartDashboard.putData("ShootBySpeed",
+				new ShootBySpeed(ConstantHandler.addConstantDouble("shooterSpeed", 300))); // inches
+																							// per
+																							// second
+		SmartDashboard.putData("ShootByDistance", new ShootByDistance(ImageProcessingConstants.distanceToBoiler));
+		SmartDashboard.putData("ShootLinear", new ShootLinear(ImageProcessingConstants.distanceToBoiler));
+		SmartDashboard.putData("MoveForwards", new DriveTank(Robot.drivetrain, 0.2, 0.2));
+		SmartDashboard.putData("MoveBackwards", new DriveTank(Robot.drivetrain, -0.2, -0.2));
+		SmartDashboard.putData("Orient Left", new OrientToTwoTargets(() -> 0.4));
+		SmartDashboard.putData("Orient Right", new OrientToTwoTargets(() -> -0.4));
+		SmartDashboard.putData("Change to Boiler Camera",
+				new RunnableCommand(() -> ImageProcessingConstants.NETWORK_TABLE.putNumber("currentCamera",
+						OrientToBoiler.BOILER_CAMERA_ID.get())));
+		SmartDashboard.putData("Change to Gear Camera", new RunnableCommand(() -> ImageProcessingConstants.NETWORK_TABLE
+				.putNumber("currentCamera", OrientToGear.GEAR_CAMERA_ID.get())));
 
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
+	}
 }
-
