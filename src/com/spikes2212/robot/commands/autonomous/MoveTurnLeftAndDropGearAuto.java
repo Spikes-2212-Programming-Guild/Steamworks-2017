d7b9d700b27cde9a67cecd5c502cf934812c6663
Spikes-2212 <spikes.no2212@gmail.com>
2017-02-22 06:44:46 +0200

    Fixed auto according to test

--- src/com/spikes2212/robot/commands/autonomous/MoveTurnLeftAndDropGearAuto.java
+++ src/com/spikes2212/robot/commands/autonomous/MoveTurnLeftAndDropGearAuto.java
@@ -1,12 +1,10 @@
 package com.spikes2212.robot.commands.autonomous;
 
-import java.util.function.Supplier;
-
-import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcade;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTank;
 import com.spikes2212.robot.Robot;
-import com.spikes2212.robot.commands.OrientateAndMoveToGear;
+import com.spikes2212.robot.commands.orientation.OrientToGear;
 import com.spikes2212.robot.subsystems.GearDropper;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -16,18 +14,17 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class MoveTurnLeftAndDropGearAuto extends CommandGroup {
 
-	public static final Supplier<Double> turningSpeed = ConstantHandler
-			.addConstantDouble("MoveTurnLeftAndDropGearAuto-turningSpeed", -0.5);
-	public static final Supplier<Double> movingStraightTime = ConstantHandler
-			.addConstantDouble("MoveTurnLeftAndDropGearAuto-movingTime", 4);
-	public static final Supplier<Double> moveToGearTime = ConstantHandler
-			.addConstantDouble("MoveTurnLeftAndDropGearAuto-moveToGearTime", 1);
-
 	public MoveTurnLeftAndDropGearAuto() {
-		addSequential(new DriveTank(Robot.drivetrain, MoveStraightAndDropGearAuto.movingSpeed,
-				MoveStraightAndDropGearAuto.movingSpeed), movingStraightTime.get());
-		addSequential(new OrientateAndMoveToGear(turningSpeed, MoveStraightAndDropGearAuto.movingSpeed),
-				moveToGearTime.get());
+		addSequential(
+				new DriveTank(Robot.drivetrain, MoveStraightAndDropGearAuto.movingSpeed,
+						MoveStraightAndDropGearAuto.movingSpeed),
+				MoveTurnRightAndDropGearAuto.movingStraightTime.get());
+		addSequential(new OrientToGear(() -> (-MoveTurnRightAndDropGearAuto.turningSpeed.get())));
+		addSequential(new DriveArcade(Robot.drivetrain, MoveStraightAndDropGearAuto.movingSpeed, () -> 0.0),
+				MoveTurnRightAndDropGearAuto.moveToGearTime.get());
 		addSequential(new MoveLimitedSubsystem(Robot.gearDropper, GearDropper.OPENING_SPEED));
+		addParallel(new MoveLimitedSubsystem(Robot.gearDropper, 0));
+		addSequential(new DriveArcade(Robot.drivetrain, MoveStraightAndDropGearAuto.reverseSpeed, () -> 0.0),
+				MoveStraightAndDropGearAuto.reverseTime.get());
 	}
 }
