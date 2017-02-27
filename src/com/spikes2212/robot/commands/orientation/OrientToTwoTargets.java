19cae0d3d7bef837748b791fe183b785dc115232
ThinkRedstone <ittai11@gmail.com>
2017-02-27 20:11:35 +0200

    Updated constants in OrientToTwoTargets.java

--- src/com/spikes2212/robot/commands/orientation/OrientToTwoTargets.java
+++ src/com/spikes2212/robot/commands/orientation/OrientToTwoTargets.java
@@ -15,50 +15,50 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class OrientToTwoTargets extends CommandGroup {
 
-	public static final Supplier<Double> KP = ConstantHandler.addConstantDouble("OrientToTwoObjects-KP", 0);
-	public static final Supplier<Double> KI = ConstantHandler.addConstantDouble("OrientToTwoObjects-KI", 0);
-	public static final Supplier<Double> KD = ConstantHandler.addConstantDouble("OrientToTwoObjects-KD", 0);
-	public static final Supplier<Double> TOLERANCE = ConstantHandler.addConstantDouble("OrientToTwoObjects-TOLERANCE",
-			0);
+    public static final Supplier<Double> KP = ConstantHandler.addConstantDouble("OrientToTwoObjects-KP", 1.8);
+    public static final Supplier<Double> KI = ConstantHandler.addConstantDouble("OrientToTwoObjects-KI", 0.05);
+    public static final Supplier<Double> KD = ConstantHandler.addConstantDouble("OrientToTwoObjects-KD", 0);
+    public static final Supplier<Double> TOLERANCE = ConstantHandler.addConstantDouble("OrientToTwoObjects-TOLERANCE",
+            0.05);
 
-	public OrientToTwoTargets(Supplier<Double> rotateSpeedSupplier) {
-		addSequential(new RunnableCommand(() -> SmartDashboard.putBoolean("Oriented", false)));
-		addSequential(new TurnToTwoTargets(rotateSpeedSupplier));
-		addSequential(new DriveTankWithPID(Robot.drivetrain, leftOrientationSource, rightOrientationSource, 0, 0,
-				KP.get(), KI.get(), KD.get(), TOLERANCE.get()));
-		addSequential(new RunnableCommand(() -> SmartDashboard.putBoolean("Oriented", true)));
-	}
+    public OrientToTwoTargets(Supplier<Double> rotateSpeedSupplier) {
+        addSequential(new RunnableCommand(() -> SmartDashboard.putBoolean("Oriented", false)));
+        addSequential(new TurnToTwoTargets(rotateSpeedSupplier));
+        addSequential(new DriveTankWithPID(Robot.drivetrain, leftOrientationSource, rightOrientationSource, 0, 0,
+                KP.get(), KI.get(), KD.get(), TOLERANCE.get()));
+        addSequential(new RunnableCommand(() -> SmartDashboard.putBoolean("Oriented", true)));
+    }
 
-	private static PIDSource leftOrientationSource = new PIDSource() {
+    private static PIDSource leftOrientationSource = new PIDSource() {
 
-		@Override
-		public void setPIDSourceType(PIDSourceType pidSource) {
-		}
+        @Override
+        public void setPIDSourceType(PIDSourceType pidSource) {
+        }
 
-		@Override
-		public double pidGet() {
-			return -ImageProcessingConstants.TWO_OBJECTS_CENTER.get();
-		}
+        @Override
+        public double pidGet() {
+            return -ImageProcessingConstants.TWO_OBJECTS_CENTER.get();
+        }
 
-		@Override
-		public PIDSourceType getPIDSourceType() {
-			return PIDSourceType.kDisplacement;
-		}
-	};
-	private static PIDSource rightOrientationSource = new PIDSource() {
+        @Override
+        public PIDSourceType getPIDSourceType() {
+            return PIDSourceType.kDisplacement;
+        }
+    };
+    private static PIDSource rightOrientationSource = new PIDSource() {
 
-		@Override
-		public void setPIDSourceType(PIDSourceType pidSource) {
-		}
+        @Override
+        public void setPIDSourceType(PIDSourceType pidSource) {
+        }
 
-		@Override
-		public double pidGet() {
-			return ImageProcessingConstants.TWO_OBJECTS_CENTER.get();
-		}
+        @Override
+        public double pidGet() {
+            return ImageProcessingConstants.TWO_OBJECTS_CENTER.get();
+        }
 
-		@Override
-		public PIDSourceType getPIDSourceType() {
-			return PIDSourceType.kDisplacement;
-		}
-	};
+        @Override
+        public PIDSourceType getPIDSourceType() {
+            return PIDSourceType.kDisplacement;
+        }
+    };
 }
